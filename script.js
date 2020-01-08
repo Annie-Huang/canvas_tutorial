3cd97c5cbb79f543f90cd416a8b0e11a4a4a2c80
Annie Huang <annie.hn@gmail.com>
2020-01-09 10:58:10 +1100

    clearRect, fillText, strokeText

--- script.js
+++ script.js
@@ -17,4 +17,36 @@ ctx.lineWidth = 5;
 ctx.strokeStyle = 'green';
 ctx.strokeRect(100, 200, 150, 100);
 
+// clearRect()
+// usually  use to clean the whole canvas
+ctx.clearRect(25, 25, 140, 90);
+
+// fillText()
+ctx.font = '30px Arial';
+// if you don't have this, it will use the last fillStyle which is blue
+ctx.fillStyle = 'purple';
+ctx.fillText('Hello World', 400, 50);
+
+// strokeText()
+// strokeText will take currently lineWidth and strokeStyle, you need to override it if you want changes.
+ctx.lineWidth = 1;
+ctx.strokeStyle = 'orange';
+ctx.strokeText('Hello World1', 400, 100);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
