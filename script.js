ae1767027de78705a6dfb743359e2d1cbf771e78
Annie Huang <annie.hn@gmail.com>
2020-01-09 12:50:08 +1100

    Quadratric curves

--- script.js
+++ script.js
@@ -99,9 +99,17 @@ ctx.arc(centerX+80, centerY-80, 20, 0, Math.PI*2)
 ctx.stroke();
 
 
+// Quadratric curves
+ctx.moveTo(75, 25);
+ctx.quadraticCurveTo(25, 25, 25, 62.5);
+ctx.quadraticCurveTo(25, 100, 50, 100);
+ctx.quadraticCurveTo(50, 120, 30, 125);
+ctx.quadraticCurveTo(60, 120, 65, 100);
+ctx.quadraticCurveTo(125, 100, 125, 62.5);
+ctx.quadraticCurveTo(125, 25, 75, 25);
+ctx.stroke();
 
-
-
+// ====================================================================================
 
 
 
