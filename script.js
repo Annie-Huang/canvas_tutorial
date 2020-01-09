354070f01ee9ea3037f34bd01f43bf816b5379ed
Annie Huang <annie.hn@gmail.com>
2020-01-09 18:04:48 +1100

    Create event listener to manually control the doll  to go up/down/left/right

--- script.js
+++ script.js
@@ -194,9 +194,56 @@ function update() {
     requestAnimationFrame(update);
 }
 
+function moveUp() {
+    player.dy = -player.speed;
+}
+
+function moveDown() {
+    player.dy = player.speed;
+}
+
+function moveLeft() {
+    player.dx = -player.speed;
+}
+
+function moveRight() {
+    player.dx = player.speed;
+}
+
+
+function keyDown(e) {
+    console.log(e.key);
+    if (e.key === 'ArrowRight' || e.key === 'Right') {
+        moveRight();
+    } else if (e.key === 'ArrowLeft' || e.key === 'Left') {
+        moveLeft();
+    } else if (e.key === 'ArrowUp' || e.key === 'Up') {
+        moveUp();
+    } else if (e.key === 'ArrowDown' || e.key === 'Down') {
+        moveDown();
+    }
+}
+
+function keyUp(e) {
+    if (
+        e.key === 'Right' ||
+        e.key === 'ArrowRight' ||
+        e.key === 'Left' ||
+        e.key === 'ArrowLeft' ||
+        e.key === 'Up' ||
+        e.key === 'ArrowUp' ||
+        e.key === 'Down' ||
+        e.key === 'ArrowDown'
+    ) {
+        player.dx = 0;
+        player.dy = 0;
+    }
+}
 
 update();
 
+document.addEventListener('keydown', keyDown);
+document.addEventListener('keyup', keyUp);
 
 
 
