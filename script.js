082c162a8e8a4946b7f42dd02e42ce413162b226
Annie Huang <annie.hn@gmail.com>
2020-01-09 15:07:47 +1100

    Create a circle bouncing within the canvas: clearRect, requestAnimationFrame

--- script.js
+++ script.js
@@ -67,53 +67,92 @@ const ctx = canvas.getContext('2d');
 
 // ====================================================================================
 
-// Arc (circles)
-ctx.beginPath();
-
-const centerX = canvas.width / 2;
-const centerY = canvas.height / 2;
-
-// Draw head
-// ctx.arc(100, 100, 40, 0, Math.PI*2);
-ctx.arc(centerX, centerY, 200, 0, Math.PI*2);
-
-// Move to mouth
-ctx.moveTo(centerX + 100, centerY); // if you comment this out, you will see the path.
-
-// Draw mouth
-// radius = 100 is half of the outer circle which is 200. False meaning it's drawing clockwise.
-ctx.arc(centerX, centerY, 100, 0, Math.PI, false);
-
-// Move left eye
-ctx.moveTo(centerX-60, centerY-80); // if you comment this out, you will see the path.
-
-// Draw left eye
-ctx.arc(centerX-80, centerY-80, 20, 0, Math.PI*2)
-
-// Move right eye
-ctx.moveTo(centerX+100, centerY-80); // if you comment this out, you will see the path.
-
-// Draw right eye
-ctx.arc(centerX+80, centerY-80, 20, 0, Math.PI*2)
-
-ctx.stroke();
-
-
-// Quadratric curves
-ctx.moveTo(75, 25);
-ctx.quadraticCurveTo(25, 25, 25, 62.5);
-ctx.quadraticCurveTo(25, 100, 50, 100);
-ctx.quadraticCurveTo(50, 120, 30, 125);
-ctx.quadraticCurveTo(60, 120, 65, 100);
-ctx.quadraticCurveTo(125, 100, 125, 62.5);
-ctx.quadraticCurveTo(125, 25, 75, 25);
-ctx.stroke();
+// // Arc (circles)
+// ctx.beginPath();
+//
+// const centerX = canvas.width / 2;
+// const centerY = canvas.height / 2;
+//
+// // Draw head
+// // ctx.arc(100, 100, 40, 0, Math.PI*2);
+// ctx.arc(centerX, centerY, 200, 0, Math.PI*2);
+//
+// // Move to mouth
+// ctx.moveTo(centerX + 100, centerY); // if you comment this out, you will see the path.
+//
+// // Draw mouth
+// // radius = 100 is half of the outer circle which is 200. False meaning it's drawing clockwise.
+// ctx.arc(centerX, centerY, 100, 0, Math.PI, false);
+//
+// // Move left eye
+// ctx.moveTo(centerX-60, centerY-80); // if you comment this out, you will see the path.
+//
+// // Draw left eye
+// ctx.arc(centerX-80, centerY-80, 20, 0, Math.PI*2)
+//
+// // Move right eye
+// ctx.moveTo(centerX+100, centerY-80); // if you comment this out, you will see the path.
+//
+// // Draw right eye
+// ctx.arc(centerX+80, centerY-80, 20, 0, Math.PI*2)
+//
+// ctx.stroke();
+//
+//
+// // Quadratric curves
+// ctx.moveTo(75, 25);
+// ctx.quadraticCurveTo(25, 25, 25, 62.5);
+// ctx.quadraticCurveTo(25, 100, 50, 100);
+// ctx.quadraticCurveTo(50, 120, 30, 125);
+// ctx.quadraticCurveTo(60, 120, 65, 100);
+// ctx.quadraticCurveTo(125, 100, 125, 62.5);
+// ctx.quadraticCurveTo(125, 25, 75, 25);
+// ctx.stroke();
 
 // ====================================================================================
 
+// Animation 1
+const circle = {
+    x: 200,
+    y: 200,
+    size: 30,
+    dx: 5,
+    dy: 4
+};
 
+function drawCircle() {
+    ctx.beginPath();
+    ctx.arc(circle.x, circle.y, circle.size, 0, Math.PI * 2);
+    ctx.fillStyle = 'purple';
+    ctx.fill();
+}
 
+function update() {
+    ctx.clearRect(0,0, canvas.width, canvas.height);
 
+    drawCircle();
+
+    // change position
+    circle.x += circle.dx;
+    circle.y += circle.dy;
+
+    // Detect side walls
+    if (circle.x + circle.size > canvas.width || circle.x - circle.size < 0 ) {
+        // console.log('HIT');
+        circle.dx *= -1; // circle.dx = circle.dx * -1;
+    }
+
+    // Detect top[ and bottom walls
+    if (circle.y + circle.size > canvas.height || circle.y - circle.size < 0 ) {
+        circle.dy *= -1;
+    }
+
+    // this is a very cool function, it will cause it to repaint all the time.
+    requestAnimationFrame(update);
+    // console.log(123); // You can see it is being called all the time.
+}
+
+update();
 
 
 
